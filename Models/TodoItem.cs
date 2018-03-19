9770f9219dc3d0b6a30ccf67db515334c3ffe33f
RamuljAdnan <adnanramulj@gmail.com>
2018-03-19 13:57:32 +0100

    Add project files.

---
+++ Models/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreToDo.Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+        public bool isDone { get; set; }
+        public string Title { get; set; }
+        public DateTimeOffset? DueAt {get;set;}
+
+
+    }
+}
---
+++ Models/TodoViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreToDo.Models
+{
+    public class TodoViewModel
+    {
+        /*Zbog toga sto u modelu imam classu za jedan objekat to-do, ovdje imam listu objekata Todo tako da korisnik moze imati vise instanci*/
+        public IEnumerable<TodoItem> Items { get; set; }
+
+    }
+}
